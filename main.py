ba95e8c7b5831b6d02d8d84fec63d13c92629748
Rishit Jakharia <113205978+RISHIT7@users.noreply.github.com>
2024-01-22 16:46:04 +0530

    Update main.py
    txt, pickle, parquet, hdf5, feather done

--- main.py
+++ main.py
@@ -1,47 +1,91 @@
 import sys
 from datetime import date
+import os
+import timeit
 from jugaad_data.nse import bhavcopy_save, bhavcopy_fo_save
 import pandas as pd
-import timeit
-import os
-
+# pyarrow, h5py, tables -> (pytables), feather-format -> (feather)
 
 # ------------------------------------ generation of data frame --------------------------------------------
 def generate_dataframe(to_date, symbol):
     from jugaad_data.nse import stock_df
-    df = pd.DataFrame(stock_df(symbol=symbol, from_date=date(to_date,1,16), to_date=date(2023,1,16), series="EQ"))
+    df = pd.DataFrame(stock_df(symbol=symbol[:-4], from_date=date(to_date,1,16), to_date=date(2023,1,16), series="EQ"))
     df = df[[ "DATE", "OPEN", "CLOSE", "HIGH","LOW", "LTP", "VOLUME", "VALUE","NO OF TRADES"]]
     return df
 
-
 # ----------------------------------------------- CSV --------------------------------------------------------
 def write_csv(DATA, symbol):
-    DATA.to_csv(path_or_buf = symbol + "_sol.csv" )
+    DATA.to_csv(path_or_buf = symbol + ".csv")
 def read_csv(symbol):
-    return pd.read_csv(symbol + "_sol.csv")
+    return pd.read_csv(symbol + ".csv")
     
-    
+# ----------------------------------------------- txt --------------------------------------------------------
+def write_txt(DATA, symbol):
+    DATA.to_csv(path_or_buf = symbol + ".txt" , sep = "\t")
+def read_txt(symbol):
+    return pd.read_csv(symbol + ".txt", sep= "\t")
+
+# ----------------------------------------------- pickle --------------------------------------------------------
+def write_pickle(DATA, symbol):
+    pd.to_pickle(DATA, symbol + ".pkl")
+def read_pickle(symbol):
+    return pd.read_pickle(symbol + ".pkl")
+
+# ----------------------------------------------- parquet --------------------------------------------------------
+def write_parquet(DATA, symbol):
+    DATA.to_parquet(symbol + ".parquet")
+def read_parquet(symbol):
+    return pd.read_parquet(symbol + ".parquet")
+
+# ----------------------------------------------- h5 --------------------------------------------------------
+def write_hdf5(DATA, symbol):
+    DATA.to_hdf(symbol + ".h5", key = 'data', mode = 'w')
+def read_hdf5(symbol):
+    return pd.read_hdf(symbol + ".h5", key = 'data')
+
+# ----------------------------------------------- feather --------------------------------------------------------
+def write_feather(DATA, symbol):
+    DATA.to_feather(symbol + ".feather")
+def read_feather(symbol):
+    return pd.read_feather(symbol + ".feather")
+
+# -------------------------------------------------------- MAIN -----------------------------------------------------------------------
 def main():
     to_date = 2023-int(sys.argv[2])
-    symbol = sys.argv[1]
+    argument = sys.argv[1]
+    symbol = argument + "_sol"
 
     DATA = generate_dataframe(to_date, symbol)
 
     # ------------------------------------------------------ CSV ----------------------------------------------------------------------
-    
     avg_csv_write_time = timeit.timeit(stmt=lambda: write_csv(DATA, symbol), number=10)
     avg_csv_read_time = timeit.timeit(stmt=lambda: read_csv(symbol), number=10) # in sec
-    avg_csv_size = os.path.getsize(symbol + "_sol.csv") # in 1000 kb
+    avg_csv_size = os.path.getsize(symbol + ".csv") # in 1000 kb
     
-    # txt
+    # ----------------------------------------------------- txt -----------------------------------------------------------------------
+    avg_txt_write_time = timeit.timeit(stmt=lambda: write_txt(DATA, symbol), number=10)
+    avg_txt_read_time = timeit.timeit(stmt=lambda: read_txt(symbol), number=10) # in sec
+    avg_txt_size = os.path.getsize(symbol + ".txt") # in 1000 kb  
+        
+    # ------------------------------------------------------ pickle -------------------------------------------------------------------
+    avg_pickle_write_time = timeit.timeit(stmt=lambda: write_pickle(DATA, symbol), number=10)
+    avg_pickle_read_time = timeit.timeit(stmt=lambda: read_pickle(symbol), number=10) # in sec
+    avg_pickle_size = os.path.getsize(symbol + ".pkl") # in 1000 kb 
     
-    # bin
+    # ------------------------------------------------------ Parquet ------------------------------------------------------------------
+    avg_parquet_write_time = timeit.timeit(stmt=lambda: write_parquet(DATA, symbol), number=10)
+    avg_parquet_read_time = timeit.timeit(stmt=lambda: read_parquet(symbol), number=10) # in sec
+    avg_parquet_size = os.path.getsize(symbol + ".parquet") # in 1000 kb 
     
-    # Parquet
+    # ------------------------------------------------------ HDF5 ------------------------------------------------------------------
+    avg_hdf5_write_time = timeit.timeit(stmt=lambda: write_hdf5(DATA, symbol), number=10)
+    avg_hdf5_read_time = timeit.timeit(stmt=lambda: read_hdf5(symbol), number=10) # in sec
+    avg_hdf5_size = os.path.getsize(symbol + ".h5") # in 1000 kb 
     
-    # HDF5
-    
-    # Feather
+    # ------------------------------------------------------ Feather ------------------------------------------------------------------
+    avg_feather_write_time = timeit.timeit(stmt=lambda: write_feather(DATA, symbol), number=10)
+    avg_feather_read_time = timeit.timeit(stmt=lambda: read_feather(symbol), number=10) # in sec
+    avg_feather_size = os.path.getsize(symbol + ".feather") # in 1000 kb 
     
     # JSON
     
